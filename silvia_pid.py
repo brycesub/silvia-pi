d76721a42f3a39a182e0e9bdfdef5c719abc75fa
Bryce Thomas <brycesub@gmail.com>
2016-01-21 11:42:54 -0600

    Changed to Adafruit GPIO

--- silvia_pid.py
+++ silvia_pid.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import time
-import RPi.GPIO as gpio
 import sys
 import math
+import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MAX31855.MAX31855 as MAX31855
 import PID as PID
@@ -16,16 +16,15 @@ SPI_PORT   = 0
 SPI_DEVICE = 0
 sensor = MAX31855.MAX31855(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
-gpio.setmode(gpio.BCM)
-gpio.setwarnings(False)
+rGPIO = GPIO.get_platform_gpio()
 
-boilerpin=4
-gpio.setup(boilerpin, gpio.OUT)
-gpio.output(boilerpin,0)
+boilerpin=26
+rGPIO.setup(boilerpin, GPIO.OUT)
+rGPIO.output(boilerpin,0)
 
 nanct=0
 
-settemp=200.
+settemp=201.
 sampletime=0.1
 P=3.0
 I=0.01
@@ -40,20 +39,21 @@ j=0
 
 # mainLoop
 try:
-  while True :
+  while True : # Loops 10x/second
     tempc = sensor.readTempC()
     tempf = c_to_f(tempc)
 
     if math.isnan(tempc) :
       nanct += 1
-#      print ' nanct:',nanct
-#      if nanct > 1000 :
-#        break
+#     print ' nanct:',nanct
+      if nanct > 100000 :
+        rGPIO.output(boilerpin,0)
+        break
       continue
     else:
       nanct = 0
 
-    print ' tempc:',tempc,'*C'
+#   print ' tempc:',tempc,'*C'
     print ' tempf:',tempf,'*F'
 
     pid.update(tempf)
@@ -61,19 +61,19 @@ try:
     print 'pidout:',pidout
 
     if pidout >= 100 :
-      gpio.output(boilerpin,1)
+      rGPIO.output(boilerpin,1)
       print 'boiler: on'
     elif pidout > 0 and pidout < 100 and tempf < settemp * 1.01 :
       if i%10 == 0 :
         j=int(pidout)/10
       if i%10 <= j :
-        gpio.output(boilerpin,1)
+        rGPIO.output(boilerpin,1)
         print 'boiler: on'
       else :
-        gpio.output(boilerpin,0)
+        rGPIO.output(boilerpin,0)
         print 'boiler: off'
     else:
-      gpio.output(boilerpin,0)
+      rGPIO.output(boilerpin,0)
       print 'boiler: off'
 
     print
@@ -84,5 +84,5 @@ try:
 
 finally:
   pid.clear
-  gpio.output(boilerpin,0)
-  gpio.cleanup()
+  rGPIO.output(boilerpin,0)
+  rGPIO.cleanup()
