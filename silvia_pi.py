80ff41af8214da6a083d90053416c011edc6278a
Bryce Thomas <brycesub@gmail.com>
2016-01-29 17:23:02 -0600

    Added multithreading, WebServer, and /curtemp /allstats and /settemp

--- silvia_pi.py
+++ silvia_pi.py
@@ -1,81 +1,128 @@
 #!/usr/bin/python
-import time
-import sys
-import math
-import Adafruit_GPIO as GPIO
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MAX31855.MAX31855 as MAX31855
-import PID as PID
-import config as conf
 
-# Define a function to convert celsius to fahrenheit.
-def c_to_f(c):
-        return c * 9.0 / 5.0 + 32.0
+from multiprocessing import Process, Manager
 
-sensor = MAX31855.MAX31855(spi=SPI.SpiDev(conf.spi_port, conf.spi_dev))
+def pid_loop(dummy,state):
+  import time
+  import sys
+  import math
+  import Adafruit_GPIO as GPIO
+  import Adafruit_GPIO.SPI as SPI
+  import Adafruit_MAX31855.MAX31855 as MAX31855
+  import PID as PID
+  import config as conf
 
-rGPIO = GPIO.get_platform_gpio()
+  def c_to_f(c):
+    return c * 9.0 / 5.0 + 32.0
 
-rGPIO.setup(conf.he_pin, GPIO.OUT)
-rGPIO.output(conf.he_pin,0)
+  sensor = MAX31855.MAX31855(spi=SPI.SpiDev(conf.spi_port, conf.spi_dev))
 
-nanct=0
-
-pid = PID.PID(conf.P,conf.I,conf.D)
-pid.SetPoint = conf.set_temp
-pid.setSampleTime(conf.sample_time)
-
-i=0
-j=0
-pidhist = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.]
-avgpid = 0
-hestat = 0
-
-print 'P =',conf.P,'I =',conf.I,'D =',conf.D,'Set Temp =',conf.set_temp
-print 'i tempf pidout pidavg pterm iterm dterm hestat'
-
-# mainLoop
-try:
-  while True : # Loops 10x/second
-    tempc = sensor.readTempC()
-    tempf = c_to_f(tempc)
-
-    if math.isnan(tempc) :
-      nanct += 1
-#     print ' nanct:',nanct
-      if nanct > 100000 :
-        rGPIO.output(conf.he_pin,0)
-        break
-      continue
-    else:
-      nanct = 0
-
-    pid.update(tempf)
-    pidout = pid.output
-    pidhist[i%10] = pidout
-    avgpid = sum(pidhist)/len(pidhist)
-
-    if avgpid >= 100 :
-      hestat = 1
-    elif avgpid > 0 and avgpid < 100 and tempf < conf.set_temp * 1.01 :
-      if i%10 == 0 :
-        j=int((avgpid/10)+.5)
-      if i%10 <= j :
-        hestat = 1
-      else :
-        hestat = 0
-    else:
-      hestat = 0
-
-    rGPIO.output(conf.he_pin,hestat) 
-    
-    print i,round(tempf,2),round(pidout,2),round(avgpid,2),round(pid.PTerm,2),round(pid.ITerm*conf.I,2),round(pid.DTerm*conf.D,2),hestat
-
-    i += 1
-
-    time.sleep(conf.sample_time)
-
-finally:
-  pid.clear
+  rGPIO = GPIO.get_platform_gpio()
+  rGPIO.setup(conf.he_pin, GPIO.OUT)
   rGPIO.output(conf.he_pin,0)
-  rGPIO.cleanup()
+
+  pid = PID.PID(conf.P,conf.I,conf.D)
+  pid.SetPoint = conf.set_temp
+  pid.setSampleTime(conf.sample_time)
+
+  nanct=0
+  i=0
+  j=0
+  pidhist = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.]
+  avgpid = 0
+  hestat = 0
+
+  print 'P =',conf.P,'I =',conf.I,'D =',conf.D,'Set Temp =',conf.set_temp
+  print 'i tempf pidout pidavg pterm iterm dterm hestat'
+
+  try:
+    while True : # Loops 10x/second
+      tempc = sensor.readTempC()
+      tempf = c_to_f(tempc)
+
+      if math.isnan(tempc) :
+        nanct += 1
+#       print ' nanct:',nanct
+        if nanct > 100000 :
+          rGPIO.output(conf.he_pin,0)
+          break
+        continue
+      else:
+        nanct = 0
+
+      pid.update(tempf)
+      pidout = pid.output
+      pidhist[i%10] = pidout
+      avgpid = sum(pidhist)/len(pidhist)
+
+      if avgpid >= 100 :
+        hestat = 1
+      elif avgpid > 0 and avgpid < 100 and tempf < conf.set_temp * 1.01 :
+        if i%10 == 0 :
+          j=int((avgpid/10)+.5)
+        if i%10 <= j :
+          hestat = 1
+        else :
+          hestat = 0
+      else:
+        hestat = 0
+
+      rGPIO.output(conf.he_pin,hestat) 
+
+      state['i'] = i
+      state['tempf'] = round(tempf,2)
+      state['pidval'] = round(pidout,2)
+      state['avgpid'] = round(avgpid,2)
+      state['pterm'] = round(pid.PTerm,2)
+      state['iterm'] = round(pid.ITerm * conf.I,2)
+      state['dterm'] = round(pid.DTerm * conf.D,2)
+      state['settemp'] = round(conf.set_temp,2)
+      state['hestat'] = hestat
+
+      print state
+
+      i += 1
+      time.sleep(conf.sample_time)
+
+  finally:
+    pid.clear
+    rGPIO.output(conf.he_pin,0)
+    rGPIO.cleanup()
+
+def rest_server(dummy,state):
+  from bottle import route, run, template, get, post, request
+
+  @route('/curtemp')
+  def curtemp():
+    return str(state['tempf'])
+
+  @get('/settemp')
+  def settemp():
+    return str(state['settemp'])
+
+  @post('/settemp')
+  def post_settemp():
+    settemp = request.forms.get('settemp')
+    if settemp > 200 and settemp < 265 :
+      state['settemp'] = settemp
+      return str(settemp)
+    return str(-1)
+
+  @route('/allstats')
+  def allstats():
+    return dict(state)
+
+  run(host='0.0.0.0',port=8080)
+
+if __name__ == '__main__':
+  manager = Manager()
+  pidstate = manager.dict()
+
+  p = Process(target=pid_loop,args=(1,pidstate))
+  p.start()
+
+  r = Process(target=rest_server,args=(1,pidstate))
+  r.start()
+
+  p.join()
+  r.join()
