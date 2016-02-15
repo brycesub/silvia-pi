9cf65c8e1c4b374ea7fb872d4913d9dab1b0e24d
Bryce Thomas <brycesub@gmail.com>
2016-02-14 23:31:49 -0600

    tweaked P, I, D

--- config.py
+++ config.py
@@ -17,9 +17,9 @@ snooze = '07:00'
 sample_time = 0.1
 
 # PID Proportional, Integral, and Derivative values
-P = 3.0
-I = 0.2
-D = 15.0
+P = 3.5
+I = 0.25
+D = 19.0
 
 #Web/REST Server Options
 port = 8080
