245dc56b7a04ea25c3a1ba1b197bcb629df8fcdd
Billy <34207387+PouryaParsa@users.noreply.github.com>
2023-12-29 22:26:41 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,28 +4,33 @@
 spi_port = 0
 spi_dev = 0
 
-# Pin # for relay connected to heating element
-he_pin = 26
+# Pin number for relay connected to heating element
+heating_element_pin = 26
 
 # Default goal temperature
-set_temp = 221.
+default_goal_temperature = 221.0
 
 # Default sleep/wake times
-sched_enabled = True
+scheduled_enabled = True
 wake_time = '06:30'
 sleep_time = '10:00'
 
 # Main loop sample rate in seconds
-sample_time = 0.1
+sample_rate = 0.1
 
-# PID Proportional, Integral, and Derivative values
-Pc = 3.4
-Ic = 0.3
-Dc = 40.0
+# PID Proportional, Integral, and Derivative values for cooling
+cooling_pid_constants = {
+    'P': 3.4,
+    'I': 0.3,
+    'D': 40.0
+}
 
-Pw = 2.9
-Iw = 0.3
-Dw = 40.0
+# PID Proportional, Integral, and Derivative values for warming
+warming_pid_constants = {
+    'P': 2.9,
+    'I': 0.3,
+    'D': 40.0
+}
 
-#Web/REST Server Options
-port = 8080
+# Web/REST Server Options
+server_port = 808
